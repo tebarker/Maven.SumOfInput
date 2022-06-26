c68504f372657bfad236eee2a2c3524836e575c2
Tenae <tenae@zipcodersmbp107.myfiosgateway.com>
2022-06-25 22:04:33 -0400

    final product

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input number: ");
 
+        int userInput = input.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= userInput; i++) {
+
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
